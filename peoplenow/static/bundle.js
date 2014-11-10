49ed758fe10c368460d6d3c3e607e3e6f51a1267
Lavrinenko Nick <nickolaylavrinenko@gmail.com>
2014-11-10 18:38:55 +0800

    switched to another ejs loader

--- peoplenow/static/bundle.js
+++ peoplenow/static/bundle.js
@@ -13526,14 +13526,36 @@
 /* 19 */
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = function (obj) {
-	obj || (obj = {});
-	var __t, __p = '', __e = _.escape;
-	with (obj) {
-	__p += '\n<div style="width: 100px; height: 100px; background: red;">\n\tHello from template\n</div>';
-
-	}
-	return __p
+	module.exports = function anonymous(locals, filters, escape, rethrow) {
+	    escape = escape || function(html) {
+	        return String(html).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/'/g, "&#39;").replace(/"/g, "&quot;");
+	    };
+	    var __stack = {
+	        lineno: 1,
+	        input: '\n<div style="width: 100px; height: 100px; background: red;">\n	Hello from template\n</div>',
+	        filename: undefined
+	    };
+	    function rethrow(err, str, filename, lineno) {
+	        var lines = str.split("\n"), start = Math.max(lineno - 3, 0), end = Math.min(lines.length, lineno + 3);
+	        var context = lines.slice(start, end).map(function(line, i) {
+	            var curr = i + start + 1;
+	            return (curr == lineno ? " >> " : "    ") + curr + "| " + line;
+	        }).join("\n");
+	        err.path = filename;
+	        err.message = (filename || "ejs") + ":" + lineno + "\n" + context + "\n\n" + err.message;
+	        throw err;
+	    }
+	    try {
+	        var buf = [];
+	        with (locals || {}) {
+	            (function() {
+	                buf.push('\n<div style="width: 100px; height: 100px; background: red;">\n	Hello from template\n</div>');
+	            })();
+	        }
+	        return buf.join("");
+	    } catch (err) {
+	        rethrow(err, __stack.input, __stack.filename, __stack.lineno);
+	    }
 	}
 
 /***/ }
--- peoplenow/webpack.config.js
+++ peoplenow/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.css$/, loader: "style!css" },
-            { test: /\.ejs$/, loader: "ejs" }
+            { test: /\.ejs$/, loader: "ejs-compiled" },
         ]
     }
 };
